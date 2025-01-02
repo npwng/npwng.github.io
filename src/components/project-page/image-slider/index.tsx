a096788e4d331b1501ac3b23e3ce85809125eade
Naidanacami <nicholas.probstwong@tutanota.com>
2025-01-02 16:36:09 -0700

    Added project page animations. Fixed arrow alignment.

--- src/components/project-page/image-slider/index.tsx
+++ src/components/project-page/image-slider/index.tsx
@@ -22,12 +22,6 @@ const dotsContainerStyles: React.CSSProperties = {
   justifyContent: "center",
 };
 
-const dotStyle: React.CSSProperties = {
-  margin: "0 3px",
-  cursor: "pointer",
-  fontSize: "20px",
-};
-
 const ImageSlider: React.FC<ImageSliderProps> = ({ slides }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -50,33 +44,33 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ slides }) => {
   return (
     <div className="w-full">
       <div style={{opacity:0}}>
-          ●
+          ●'
       </div>
       <div className="flex w-full" style={flexStyle}>
-        <div onClick={goToPrevious}>
-          <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="currentColor">
-            <path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" />
+        <div className="py-2 px-2 cursor-pointer transform transition-transform duration-800 hover:scale-105" onClick={goToPrevious}>
+          <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="currentColor">
+            <path d="M17.77 3.77 16 2 6 12l10 10 1.77-1.77L9.54 12z" />
           </svg>
         </div>
         <div style={{ ...slideStyles, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
           <img src={slides[currentIndex]} alt={`Slide ${currentIndex}`} style={{ borderRadius: '10px', maxWidth: '100%', maxHeight: '25rem' }} />
         </div>
-        <div onClick={goToNext}>
-          <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="currentColor" transform="rotate(180)">
-            <path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" />
+        <div className="py-2 px-2 cursor-pointer transform transition-transform duration-800 hover:scale-105" onClick={goToNext}>
+          <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="currentColor">
+            <path d="M6.23 20.23 8 22l10-10L8 2 6.23 3.77 14.46 12z" />
           </svg>
         </div>
       </div>
 
-      <div style={dotsContainerStyles}>
+      <div style={dotsContainerStyles}> 
         {slides.map((_, slideIndex) => (
           <div
-            style={dotStyle}
-            className={`${dotStyle.cursor} ${dotStyle.fontSize} ${slideIndex === currentIndex ? 'text-primary' : 'text-base-content'} text-opacity-60`}
+            className={`${slideIndex === currentIndex ? 'text-primary' : 'text-base-content'} text-opacity-60 transform transition-transform duration-800 hover:scale-110 cursor-pointer`}
             key={slideIndex}
+            style={{marginTop: '0.75rem'}}
             onClick={() => goToSlide(slideIndex)}
           >
-            ●
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 24 24" width="1.75em" fill="currentColor"><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2"></path></svg>
           </div>
         ))}
       </div>
--- src/components/project-page/index.tsx
+++ src/components/project-page/index.tsx
@@ -31,12 +31,12 @@ const ProjectCard = ({
                         <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="currentColor"> <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/></svg>
                     </a>
                 </div>
-                <a className='card shadow-lg compact bg-base-100 cursor-pointer w-full'> 
+                <div className='card shadow-lg compact bg-base-100 w-full'> 
                     <div className='pace-x-4 flex pl-6 pr-6 py-4 w-full'>
                         <ImageSlider slides={slides} />
                     </div>
-                </a>
-                <div className='card shadow-lg compact bg-base-100 cursor-pointer w-full'> 
+                </div>
+                <div className='card shadow-lg compact bg-base-100 w-full'> 
                     <div className='flex pl-6 pr-6 py-6'>
                         {matchingProject?.body}
                     </div>
